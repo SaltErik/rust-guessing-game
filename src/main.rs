4a6216324b2cce2c3648c38885d6dea47403bc0a
Erik Nygren <erik.nygren@appliedtechnology.se>
2019-12-13 13:58:38 +0100

    refactored if into match

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-  let mut attempts_left = 10;
+  let mut tries_remaining: u8 = 10;
 
-  let secret_number = rand::thread_rng()
-    .gen_range(1, 101); // lower inclusive, upper exclusive
-  
+  let secret_number: u8 = rand::thread_rng().gen_range(1, 101); // lower inclusive, upper exclusive
+
   println!("I'm thinking of a number between 1 and 100...");
   println!("Can you guess what it is?");
 
   loop {
     let mut input = String::new();
-    
+
     io::stdin()
       .read_line(&mut input)
       .expect("ERROR: Failed to read line.");
-    
-    let guess: u32 = match input
-      .trim()
-      .parse::<u32>() {
-        Ok(num) => num,
-        Err(_) => continue,
-      };
+
+    let guess: u8 = match input.trim().parse::<u8>() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
     match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Too small!"),
       Ordering::Greater => println!("Too big!"),
       Ordering::Equal => {
         println!("You win!");
         break;
       }
+      Ordering::Less => println!("Too small!"),
     }
 
-    attempts_left -= 1;
+    tries_remaining -= 1;
 
-    if attempts_left > 1 {
-      println!("{} guesses remaining!", attempts_left);
-    } else if attempts_left == 1 {
-      println!("{} guess remaining!", attempts_left);
-    } else {
-      println!("Out of guesses! Game over!");
-      break;
+    match tries_remaining.cmp(&1) {
+      Ordering::Greater => println!("{} guesses remaining!", tries_remaining),
+      Ordering::Equal => println!("{} guess remaining!", tries_remaining),
+      Ordering::Less => {
+        println!("Out of guesses! Game over!");
+        break;
+      }
     }
   }
 }
