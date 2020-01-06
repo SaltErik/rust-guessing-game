fd3e51194aa536f9563eb062772903946bc3c05a
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-01-06 19:17:02 +0100

    guesses can panic

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,42 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn main() {
-  let mut tries_remaining: u8 = 10;
+#[derive(Eq)]
+pub struct Guess {
+  value: i32,
+}
 
-  let secret_number: u8 = rand::thread_rng().gen_range(1, 101); // lower inclusive, upper exclusive
+impl Guess {
+  pub fn new(value: i32) -> Guess {
+    if value < 1 || value > 100 {
+      panic!("Guess value must be between 1 and 100, got {}.", value);
+    }
+    Guess { value }
+  }
+}
+
+impl Ord for Guess {
+  fn cmp(&self, other: &Self) -> Ordering {
+      self.value.cmp(&other.value)
+  }
+}
+
+impl PartialOrd for Guess {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+      Some(self.cmp(other))
+  }
+}
+
+impl PartialEq for Guess {
+  fn eq(&self, other: &Self) -> bool {
+      self.value == other.value
+  }
+}
+
+fn main() {
+  let mut tries_remaining = 10;
+
+  let secret_number = Guess::new(rand::thread_rng().gen_range(1, 101)); // lower inclusive, upper exclusive
 
   println!("I'm thinking of a number between 1 and 100...");
   println!("Can you guess what it is?");
@@ -17,8 +49,8 @@ fn main() {
       .read_line(&mut input)
       .expect("ERROR: Failed to read line.");
 
-    let guess: u8 = match input.trim().parse() {
-      Ok(num) => num,
+    let guess = match input.trim().parse() {
+      Ok(num) => Guess::new(num),
       Err(_) => continue,
     };
 
